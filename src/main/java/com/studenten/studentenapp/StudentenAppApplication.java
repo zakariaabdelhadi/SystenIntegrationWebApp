3dc574ad96573dea9ac4ede915e7292611d86380
zakaria <zakizermani@gmail.com>
2023-02-16 23:36:34 +0100

    adding the add-students Page

--- src/main/java/com/studenten/studentenapp/StudentenAppApplication.java
+++ src/main/java/com/studenten/studentenapp/StudentenAppApplication.java
@@ -1,15 +1,32 @@
 package com.studenten.studentenapp;
 
+import com.studenten.studentenapp.entities.Student;
+import com.studenten.studentenapp.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 
-public class StudentenAppApplication {
+public class StudentenAppApplication implements CommandLineRunner {
+
+
 
 
     public static void main(String[] args) {
         SpringApplication.run(StudentenAppApplication.class, args);
     }
 
+    @Autowired
+    private StudentRepository studentRepository;
+    @Override
+    public void run(String... args) throws Exception {
+
+      //  Student student1 = new Student("Milissa","zermani","kouider@issani.com");
+       // studentRepository.save(student1);
+       // Student student2 = new Student("sofia","zermani","kouider@issani.com");
+       // studentRepository.save(student2);
+
+    }
 }
--- src/main/java/com/studenten/studentenapp/controller/StudentController.java
+++ src/main/java/com/studenten/studentenapp/controller/StudentController.java
@@ -1,9 +1,12 @@
 package com.studenten.studentenapp.controller;
 
+import com.studenten.studentenapp.entities.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import com.studenten.studentenapp.service.StudentService;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -20,6 +23,25 @@ public class StudentController {
 
         model.addAttribute("students", studentService.getAll());
         return "students";
+
+    }
+
+
+    @GetMapping("/students/new")
+    public String createStudentForm(Model model){
+
+        Student student = new Student();
+        model.addAttribute("student",student);
+        return "create_student";
+
+    }
+
+    @PostMapping("/saveStudent")
+    public String saveStudent(@ModelAttribute("student") Student student){
+
+        studentService.saveStudent(student);
+        return  "redirect:/students";
+
 
     }
 }
