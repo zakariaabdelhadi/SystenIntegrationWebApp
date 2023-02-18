9639d440b1ae847ac96bbf7dd7f100a30a1e980a
ZakariaZermani <43765819+zakariaabdelhadi@users.noreply.github.com>
2023-02-18 13:22:03 +0100

    Update StudentenAppApplicationTests.java

--- src/test/java/com/studenten/studentenapp/StudentenAppApplicationTests.java
+++ src/test/java/com/studenten/studentenapp/StudentenAppApplicationTests.java
@@ -3,11 +3,11 @@ package com.studenten.studentenapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class StudentenAppApplicationTests {
+@SpringBootTest
+class StudentenAppApplicationTests {
 
-//    @Test
-  //  void contextLoads() {
-    //}
+    @Test
+    void contextLoads() {
+    }
 
 }
